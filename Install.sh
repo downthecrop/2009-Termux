c4c143c86ea58e93ceb10f6082bd28ff652e587c
downthecrop <bryce@mathx.ca>
2021-09-30 18:51:28 -0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,6 +3,7 @@ apt-get --yes --force-yes install default-jre tightvncserver fluxbox git
 git clone https://github.com/downthecrop/2009-Termux
 cd 2009-Termux
 mv * ../
+cd ../
 rm -rf 2009-Termux/
 chmod +x run.sh
 chmod +x clean.sh
